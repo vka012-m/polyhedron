821ac571dd5592de6a3d606e7ddb3154febebb62
vka012-m <vkirill2006@inbox.ru>
2025-06-01 00:11:26 +0300

    Update run_shadow.py

--- run_shadow.py
+++ run_shadow.py
@@ -4,16 +4,19 @@ from time import time
 from common.tk_drawer import TkDrawer
 from shadow.polyedr import Polyedr
 
-
 tk = TkDrawer()
 try:
-    for name in ["ccc", "cube", "box", "king", "cow"]:
+    for name in ["tetrahedron", "octahedron", "ccc", "cube", "box", "king", "cow"]:
         print("=============================================================")
         print(f"Начало работы с полиэдром '{name}'")
         start_time = time()
-        Polyedr(f"data/{name}.geom").draw(tk)
+        Poly = Polyedr(f"data/{name}.geom")
+        Poly.draw(tk)
         delta_time = time() - start_time
         print(f"Изображение полиэдра '{name}' заняло {delta_time} сек.")
+        print("Cумма площадей «граней с полностью видимыми рёбрами», "
+              "проекция центра которых находится "
+              "строго внутри окружности x^2+y^2=4:", Poly.sum_area)
         input("Hit 'Return' to continue -> ")
 except (EOFError, KeyboardInterrupt):
     print("\nStop")
