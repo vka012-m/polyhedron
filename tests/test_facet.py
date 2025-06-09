65ed71abf83bbe6517509a43420530d4fcaf20a6
vka012-m <vkirill2006@inbox.ru>
2025-06-09 17:34:17 +0300

    Update test_facet.py

--- tests/test_facet.py
+++ tests/test_facet.py
@@ -10,32 +10,40 @@ class TestVoid(unittest.TestCase):
     # Эта грань не является вертикальной
     def test_vertical01(self):
         edges = []
-        f = Facet([R3(0.0, 0.0, 0.0), R3(3.0, 0.0, 0.0), R3(0.0, 3.0, 0.0)], edges)
+        f = Facet([R3(0.0, 0.0, 0.0), R3(3.0, 0.0, 0.0),
+                   R3(0.0, 3.0, 0.0)], edges)
         self.assertFalse(f.is_vertical())
 
     # Эта грань вертикальна
     def test_vertical02(self):
         edges = []
-        f = Facet([R3(0.0, 0.0, 0.0), R3(0.0, 0.0, 1.0), R3(1.0, 0.0, 0.0)], edges)
+        f = Facet([R3(0.0, 0.0, 0.0), R3(0.0, 0.0, 1.0),
+                   R3(1.0, 0.0, 0.0)], edges)
         self.assertTrue(f.is_vertical())
 
     # Нормаль к этой грани направлена вертикально вверх
     def test_h_normal01(self):
         edges = []
-        f = Facet([R3(0.0, 0.0, 0.0), R3(3.0, 0.0, 0.0), R3(0.0, 3.0, 0.0)], edges)
-        self.assertEqual(R3CollinearMatcher(f.h_normal()), R3(0.0, 0.0, 1.0))
+        f = Facet([R3(0.0, 0.0, 0.0), R3(3.0, 0.0, 0.0),
+                   R3(0.0, 3.0, 0.0)], edges)
+        self.assertEqual(R3CollinearMatcher(f.h_normal()),
+                         R3(0.0, 0.0, 1.0))
 
     # Нормаль к этой грани тоже направлена вертикально вверх
     def test_h_normal02(self):
         edges = []
-        f = Facet([R3(0.0, 0.0, 0.0), R3(0.0, 3.0, 0.0), R3(3.0, 0.0, 0.0)], edges)
-        self.assertEqual(R3CollinearMatcher(f.h_normal()), R3(0.0, 0.0, 1.0))
+        f = Facet([R3(0.0, 0.0, 0.0), R3(0.0, 3.0, 0.0),
+                   R3(3.0, 0.0, 0.0)], edges)
+        self.assertEqual(R3CollinearMatcher(f.h_normal()),
+                         R3(0.0, 0.0, 1.0))
 
     # Для нахождения нормали к этой грани рекомендуется нарисовать картинку
     def test_h_normal03(self):
         edges = []
-        f = Facet([R3(1.0, 0.0, 0.0), R3(0.0, 1.0, 0.0), R3(0.0, 0.0, 1.0)], edges)
-        self.assertEqual(R3CollinearMatcher(f.h_normal()), R3(1.0, 1.0, 1.0))
+        f = Facet([R3(1.0, 0.0, 0.0), R3(0.0, 1.0, 0.0),
+                   R3(0.0, 0.0, 1.0)], edges)
+        self.assertEqual(R3CollinearMatcher(f.h_normal()),
+                         R3(1.0, 1.0, 1.0))
 
     # Для каждой из следующих граней сначала «вручную» находятся
     # внешние нормали к вертикальным плоскостям, проходящим через
@@ -45,8 +53,10 @@ class TestVoid(unittest.TestCase):
     # Нормали для треугольной грани
     def test_v_normal01(self):
         edges = []
-        f = Facet([R3(0.0, 0.0, 0.0), R3(3.0, 0.0, 0.0), R3(0.0, 3.0, 0.0)], edges)
-        normals = [R3(-1.0, 0.0, 0.0), R3(0.0, -1.0, 0.0), R3(1.0, 1.0, 0.0)]
+        f = Facet([R3(0.0, 0.0, 0.0), R3(3.0, 0.0, 0.0),
+                   R3(0.0, 3.0, 0.0)], edges)
+        normals = [R3(-1.0, 0.0, 0.0), R3(0.0, -1.0, 0.0),
+                   R3(1.0, 1.0, 0.0)]
         for t in zip(f.v_normals(), normals):
             self.assertEqual(R3CollinearMatcher(t[0]), t[1])
 
@@ -63,8 +73,10 @@ class TestVoid(unittest.TestCase):
     # Нормали для ещё одной треугольной грани
     def test_v_normal03(self):
         edges = []
-        f = Facet([R3(1.0, 0.0, 0.0), R3(0.0, 1.0, 0.0), R3(0.0, 0.0, 1.0)], edges)
-        normals = [R3(0.0, -1.0, 0.0), R3(1.0, 1.0, 0.0), R3(-1.0, 0.0, 0.0)]
+        f = Facet([R3(1.0, 0.0, 0.0), R3(0.0, 1.0, 0.0),
+                   R3(0.0, 0.0, 1.0)], edges)
+        normals = [R3(0.0, -1.0, 0.0), R3(1.0, 1.0, 0.0),
+                   R3(-1.0, 0.0, 0.0)]
         for t in zip(f.v_normals(), normals):
             self.assertEqual(R3CollinearMatcher(t[0]), t[1])
 
@@ -73,20 +85,24 @@ class TestVoid(unittest.TestCase):
         edges = []
         f = Facet([R3(0.0, 0.0, 0.0), R3(2.0, 0.0, 0.0),
                    R3(2.0, 2.0, 0.0), R3(0.0, 2.0, 0.0)], edges)
-        self.assertEqual(R3ApproxMatcher(f.center()), (R3(1.0, 1.0, 0.0)))
+        self.assertEqual(R3ApproxMatcher(f.center()),
+                         (R3(1.0, 1.0, 0.0)))
 
     # Центр треугольника
     def test_center02(self):
         edges = []
-        f = Facet([R3(0.0, 0.0, 0.0), R3(3.0, 0.0, 0.0), R3(0.0, 3.0, 0.0)], edges)
-        self.assertEqual(R3ApproxMatcher(f.center()), (R3(1.0, 1.0, 0.0)))
+        f = Facet([R3(0.0, 0.0, 0.0), R3(3.0, 0.0, 0.0),
+                   R3(0.0, 3.0, 0.0)], edges)
+        self.assertEqual(R3ApproxMatcher(f.center()),
+                         (R3(1.0, 1.0, 0.0)))
 
     # Площадь треугольника
     def test_area01(self):
         edges = [Edge(R3(0.0, 0.0, 0.0), R3(3.0, 0.0, 0.0)),
                  Edge(R3(0.0, 0.0, 0.0), R3(0.0, 3.0, 0.0)),
                  Edge(R3(3.0, 0.0, 0.0), R3(0.0, 3.0, 0.0))]
-        f = Facet([R3(0.0, 0.0, 0.0), R3(3.0, 0.0, 0.0), R3(0.0, 3.0, 0.0)], edges)
+        f = Facet([R3(0.0, 0.0, 0.0), R3(3.0, 0.0, 0.0),
+                   R3(0.0, 3.0, 0.0)], edges)
         self.assertEqual(f.area(), 4.5)
 
     # Площадь квадрата
